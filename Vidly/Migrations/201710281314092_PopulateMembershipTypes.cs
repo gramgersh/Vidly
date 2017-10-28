8fe9ed3423e31cec2db2bfde2817cfc2525ce7db
Scott Erickson <visualstudio@grandma-gershwin.com>
2017-10-28 06:17:35 -0700

    Populate the MembershipTypes table.

---
+++ Vidly/Migrations/201710281314092_PopulateMembershipTypes.cs
@@ -0,0 +1,24 @@
+namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PopulateMembershipTypes : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO MembershipTypes (Id,SignupFee, DurationInMonths,DiscountRate) VALUES(1,0,0,0)");
+            Sql("INSERT INTO MembershipTypes (Id,SignupFee, DurationInMonths,DiscountRate) VALUES(2,30,1,10)");
+            Sql("INSERT INTO MembershipTypes (Id,SignupFee, DurationInMonths,DiscountRate) VALUES(3,90,3,15)");
+            Sql("INSERT INTO MembershipTypes (Id,SignupFee, DurationInMonths,DiscountRate) VALUES(4,300,12,20)");
+        }
+        
+        public override void Down()
+        {
+            Sql("DELETE FROM MembershipTypes WHERE Id = 4");
+            Sql("DELETE FROM MembershipTypes WHERE Id = 3");
+            Sql("DELETE FROM MembershipTypes WHERE Id = 2");
+            Sql("DELETE FROM MembershipTypes WHERE Id = 1");
+        }
+    }
+}
