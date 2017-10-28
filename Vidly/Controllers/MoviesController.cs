f838fd4de644a9149f6d8fb2cd32dd8c10ca6e62
Scott Erickson <visualstudio@grandma-gershwin.com>
2017-10-28 04:32:04 -0700

    Section 1 - Add Movies, MoviesController, and Movies/Index.

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        [Route("Movies/Index")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
