c54756852d08aab5d078074392d7283204ea77df
Scott Erickson <visualstudio@grandma-gershwin.com>
2017-10-28 10:46:33 -0700

    Added a Custom Validation class for Customer.

---
+++ Vidly/Models/Min18YearsIfAMember.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    // This is a ValidationAttribute to be appled between a Customer BirthDate and the
+    // Membership type.
+    public class Min18YearsIfAMember : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var cust = (Customer)validationContext.ObjectInstance;
+            // To avoid too many red errors on page, don't print this if no membership type has been chosen.
+            if (cust.MembershipTypeId == 0 || cust.MembershipTypeId == 1)
+            {
+                return ValidationResult.Success;
+            }
+            if (cust.BirthDate == null)
+            {
+                return new ValidationResult("Birthdate is required.");
+            }
+
+            var age = DateTime.Today.Year - cust.BirthDate.Value.Year;
+            
+            if ( age >= 18 )
+            {
+                return ValidationResult.Success;
+            }
+            return new ValidationResult("Customer should be at least 18 years old to be a member.");
+        }
+    }
+}
