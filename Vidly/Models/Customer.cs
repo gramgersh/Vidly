ed22094a6a9197a14819b97a800f3c4812feed74
Scott Erickson <visualstudio@grandma-gershwin.com>
2017-10-28 06:20:24 -0700

    Add Annotations to Customers.Name to make it required and a specific length.

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -10,6 +10,8 @@ namespace Vidly.Models
     {
         public int Id { get; set; }
 
+        [Required]
+        [StringLength(50)]
         public string Name { get; set; }
 
         public bool IsSubscribedToNewsletter { get; set; }
