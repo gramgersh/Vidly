329e8c793ed76b1925516222c21a4be2154330fb
Scott Erickson <visualstudio@grandma-gershwin.com>
2017-11-09 08:55:02 -0800

    Section 10 - Added Rentals DTO, Model, and API skeleton.

---
+++ Vidly/Controllers/Api/NewRentalsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vidly.Models;
+using Vidly.Dtos;
+
+namespace Vidly.Controllers.Api
+{
+    public class NewRentalsController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public NewRentalsController ()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+            base.Dispose(disposing);
+        }
+
+        [HttpPost]
+        public IHttpActionResult CreateNewRentals(NewRentalDto newRental)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Vidly/Models/Rental.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class Rental
+    {
+        // primary key
+        public int Id { get; set; }
+
+        [Required]
+        public Customer Customer { get; set; }
+
+        [Required]
+        public Movie Movie { get; set; }
+
+        [Display(Name = "Date Rented")]
+        public DateTime DateRented { get; set; }
+
+        [Display(Name = "Date Returned")]
+        public DateTime? DateReturned { get; set; }
+    }
+}
