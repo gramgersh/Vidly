6c8690d1e58870d02ad961bc3597f0ca189b3576
Scott Erickson <visualstudio@grandma-gershwin.com>
2017-11-10 04:04:24 -0800

    Section 10 - Toastr notifications

--- Vidly/App_Start/BundleConfig.cs
+++ Vidly/App_Start/BundleConfig.cs
@@ -16,7 +16,8 @@ namespace Vidly
                       "~/Scripts/bootbox.js",
                       "~/Scripts/datatables/jquery.datatables.js",
                       "~/Scripts/datatables/datatables.bootstrap.js",
-                      "~/Scripts/typeahead.bundle.js"
+                      "~/Scripts/typeahead.bundle.js",
+                      "~/Scripts/toastr.js"
                 ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
@@ -31,7 +32,8 @@ namespace Vidly
                       "~/Content/bootstrap-lumen.css",
                       "~/Content/datatables/css/datatables.bootstrap.css",
                       "~/Content/site.css",
-                      "~/Content/typeahead.css"
+                      "~/Content/typeahead.css",
+                      "~/Content/toastr.css"
                       ));
         }
     }
